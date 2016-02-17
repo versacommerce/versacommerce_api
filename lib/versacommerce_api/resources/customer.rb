760e5d5bd4a113de07c1659e67f89b960c3afb09
Daniel Krüger <krueger@versacommerce.de>
2016-02-17 11:27:50 +0100

    Add associations for customers

--- lib/versacommerce_api/resources/customer.rb
+++ lib/versacommerce_api/resources/customer.rb
@@ -2,6 +2,15 @@
 module VersacommerceAPI
 
   class Customer < Base
+    include Associatable
+    
+    def billing_address
+      associated_resource "billing_address", false
+    end
+
+    def shipment_address
+      associated_resource "shipment_address", false
+    end
   end
   
 end
