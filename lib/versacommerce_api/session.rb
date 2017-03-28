46aae0aac5e5adee664bbddf19664f79fc8bf276
Tobias Bühlmann <buehlmann@versacommerce.de>
2017-03-28 16:47:10 +0200

    Request token via HTTPS

--- lib/versacommerce_api/session.rb
+++ lib/versacommerce_api/session.rb
@@ -31,7 +31,7 @@ module VersacommerceAPI
     def self.request_token(domain)
       return nil if domain.blank? || api_key.blank?
       begin
-        content = open("http://#{domain}/api/auth.xml?api_key=#{api_key}") { |io| data = io.read }
+        content = open("https://#{domain}/api/auth.xml?api_key=#{api_key}") { |io| data = io.read }
         Hash.from_xml(content)["token"] if content
       rescue
         nil
